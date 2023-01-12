d09eeee2321bb9487f045364ccd5276243dc8a96
ZimovOleg <zimov.oleg@gmail.com>
2023-01-12 20:52:53 +0400

    Первое задание NEW

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,3 +1,4 @@
+'use strict'
 function getArrayParams(arr) {
   let min = Infinity;
   let max = -Infinity;
@@ -5,15 +6,12 @@ function getArrayParams(arr) {
   let avg = 0;
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-    if (arr[i] < min) {
-      min = arr[i];
-    }
     sum += arr[i];
-    }
-    avg = Number((sum / arr.length).toFixed(2));
+  }
+  min = Math.min(...arr);
+  max = Math.max(...arr);
+  avg = Number((sum / arr.length).toFixed(2));
+    
     return { min: min, max: max, avg: avg };
   }
 
