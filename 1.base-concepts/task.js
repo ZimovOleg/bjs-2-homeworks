ac6b887fac8cb490aff224ad3c26174564773532
ZimovOleg <zimov.oleg@gmail.com>
2023-01-12 22:32:45 +0400

    Задание 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,36 @@
 "use strict"
+// Задание №1
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  const d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+    return arr;
+  } 
+  if (d === 0) {
+    arr.push(-b / (2 * a));
+    return arr;
+  }
+  arr.push((-b + Math.sqrt(d)) / (2 * a));
+  arr.push((-b - Math.sqrt(d)) / (2 * a));
+
+  return arr; // array
 }
+
+// Задание №2
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
+if (isNaN(percent) || isNaN(contribution) || isNaN(amount)) {
+    return false;
+  }
+  if(countMonths < 0 || amount < 0) {
+    return false;
+  } 
+  
+  let monthPercent = percent / 100 / 12;  
+  let debt = amount - contribution;       
+  let monthlyFee = debt * (monthPercent + (monthPercent / (((1 + monthPercent) ** countMonths) - 1))); 
+  let totalAmount = parseFloat((monthlyFee * countMonths).toFixed(2));
+
+  return totalAmount;
 }
