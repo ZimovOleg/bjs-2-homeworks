dbaca34468760056d5c531b0d1c42921e8cf0b08
ZimovOleg <zimov.oleg@gmail.com>
2023-01-27 15:09:16 +0400

    Задача 1 и 2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+
+  if (arr1.length === arr2.length && 
+    arr1.every((element, index) => element === arr2[index])) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+function getUsersNamesInAgeRange(user, gender) {
+    return user
+        .filter(item => item.gender === gender)
+        .reduce((acc, item, index, arr) => {
+            return acc + item.age / arr.length
+        }, 0)
 }
